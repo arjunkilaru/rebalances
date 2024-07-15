fe80d2956ce944cd090be2cf2500d96ae0e23786
arjunkilaru <88005394+arjunkilaru@users.noreply.github.com>
2024-07-15 15:22:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -476,6 +476,8 @@ def update_output(n_clicks, ticker, amount, high1):
                 df[column] = pd.to_numeric(df[column], errors='coerce')
             style_data_conditionals = []
             for column in df.columns:
+                if column == '# Day High':
+                    continue
                 strin = "{" + column + "}"
                 style_data_conditionals.append({
                     'if': {'filter_query': strin + ' > 0', 'column_id': column},
