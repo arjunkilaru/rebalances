db32ebe90715632fad6f1ef5832656791bd43ed7
arjunkilaru <88005394+arjunkilaru@users.noreply.github.com>
2024-12-23 10:54:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1285,7 +1285,7 @@ def filter_dataframe(n_clicks, selected_idx_nm, selected_idx_chg, net_adv_value,
                 style_table={
                     'overflowX': 'auto',
                     'width': '100%',  # Ensuring the table uses the full width available
-                    'minWidth': '1000px',  # Minimum width of the table to ensure all columns are visible
+                    'minWidth': '100%',  # Minimum width of the table to ensure all columns are visible
                 },                fixed_columns={'headers': True, 'data': 1},
                 style_header={
                     'fontWeight': 'bold',  # Bold header
